ed8bf2b36325d6a84d26ffca801c8b08b5489f9f
蒼時弦也 <elct9620@frost.tw>
2016-07-10 15:26:26 +0800

    Fix unit test file

--- spec/amberbot_spec.rb
+++ spec/amberbot_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Amberbot do
+describe AmberBot do
   it 'has a version number' do
-    expect(Amberbot::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(AmberBot::VERSION).not_to be nil
   end
 end
